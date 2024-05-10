4dc577859881465398d3ec53477931c142ac809e
Imamsubekti26 <imam0subekti@gmail.com>
2024-05-10 22:23:44 +0700

    import entity and setting retrofit up

---
+++ app/src/main/java/com/imamsubekti/githubuserv4/config/ApiConfig.kt
@@ -0,0 +1,39 @@
+package com.imamsubekti.githubuserv4.config
+
+import com.imamsubekti.githubuserv4.BuildConfig
+import com.imamsubekti.githubuserv4.repository.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        fun getApiService(): ApiService {
+            val client = OkHttpClient.Builder().run {
+                addInterceptor { chain ->
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+                    val request = chain
+                        .request()
+                        .newBuilder()
+                        .addHeader("Authorization", "token ${BuildConfig.API_KEY}")
+                        .build()
+
+                    chain.proceed(request)
+
+                }
+                build()
+            }
+
+            val retrofit = Retrofit.Builder().run {
+                baseUrl(BuildConfig.BASE_URL)
+                addConverterFactory(GsonConverterFactory.create())
+                client(client)
+                build()
+            }
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/imamsubekti/githubuserv4/repository/ApiService.kt
@@ -0,0 +1,24 @@
+package com.imamsubekti.githubuserv4.repository
+
+import com.imamsubekti.githubusersv2.entity.DetailResponse
+import com.imamsubekti.githubusersv2.entity.UserResponse
+import com.imamsubekti.githubusersv2.entity.SearchResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("search/users")
+    fun getSearch(@Query("q") searchKey: String): Call<SearchResponse>
+
+    @GET("users/{username}")
+    fun getDetail(@Path("username") username: String): Call<DetailResponse>
+
+    @GET("users/{username}/followers")
+    fun getFollowers(@Path("username") username: String): Call<List<UserResponse>>
+
+    @GET("users/{username}/following")
+    fun getFollowing(@Path("username") username: String): Call<List<UserResponse>>
+
+}
